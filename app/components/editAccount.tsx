bd69682c1b2e3e8dc15387b145db161de2ae7b27
Raphael Pruckner <rapru.rp@gmail.com>
2023-07-27 19:49:13 +0200

    fix: add modal wrapper elements

--- app/components/editAccount.tsx
+++ app/components/editAccount.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { account, updateAccount } from "../services/database";
 
-export default function EditAccount({ account, closeCallback }: {account: account, closeCallback: () => void}) {
+export default function EditAccount({ account, closeCallback }: { account: account, closeCallback: () => void }) {
     const [name, setName] = useState(account.id)
     const [description, setDescription] = useState(account.name)
 
@@ -24,20 +24,22 @@ export default function EditAccount({ account, closeCallback }: {account: accoun
     }
 
     return (
-        <>
-            <h1>Konto bearbeiten</h1>
-            <form onSubmit={handleUpdateAccount}>
-                <label>
-                    Name
-                    <input type="text" value={name} onChange={handleNameChange}></input>
-                </label>
-                <label>
-                    Beschreibung
-                    <input type="text" value={description} onChange={handleDescriptionChange}></input>
-                </label>
-                <button type="submit">Speichern</button>
-                <button className="abort" onClick={closeCallback}>Abbrechen</button>
-            </form>
-        </>
+        <div className="fill-screen">
+            <div className="modal">
+                <h1>Konto bearbeiten</h1>
+                <form onSubmit={handleUpdateAccount}>
+                    <label>
+                        Name
+                        <input type="text" value={name} onChange={handleNameChange}></input>
+                    </label>
+                    <label>
+                        Beschreibung
+                        <input type="text" value={description} onChange={handleDescriptionChange}></input>
+                    </label>
+                    <button type="submit">Speichern</button>
+                    <button className="abort" onClick={closeCallback}>Abbrechen</button>
+                </form>
+            </div>
+        </div>
     )
 }
--- app/components/newAccount.tsx
+++ app/components/newAccount.tsx
@@ -26,20 +26,22 @@ export default function NewAccount({ closeCallback }: { closeCallback: () => voi
     }
 
     return (
-        <>
-            <h1>Konto erstellen</h1>
-            <form onSubmit={handleAddAccount}>
-                <label>
-                    Name
-                    <input type="text" value={name} onChange={handleNameChange}></input>
-                </label>
-                <label>
-                    Beschreibung
-                    <input type="text" value={description} onChange={handleDescriptionChange}></input>
-                </label>
-                <button type="submit">Speichern</button>
-                <button className="abort" onClick={closeCallback}>Abbrechen</button>
-            </form>
-        </>
+        <div className="fill-screen">
+            <div className="modal">
+                <h1>Konto erstellen</h1>
+                <form onSubmit={handleAddAccount}>
+                    <label>
+                        Name
+                        <input type="text" value={name} onChange={handleNameChange}></input>
+                    </label>
+                    <label>
+                        Beschreibung
+                        <input type="text" value={description} onChange={handleDescriptionChange}></input>
+                    </label>
+                    <button type="submit">Speichern</button>
+                    <button className="abort" onClick={closeCallback}>Abbrechen</button>
+                </form>
+            </div>
+        </div>
     )
 }
