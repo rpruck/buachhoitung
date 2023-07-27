b04a7ba1c6bc4162dfa3f027791471490823f66f
Raphael Pruckner <rapru.rp@gmail.com>
2023-07-27 17:27:04 +0200

    fix: reset and close newAccount modal after adding an account

--- app/components/newAccount.tsx
+++ app/components/newAccount.tsx
@@ -20,6 +20,9 @@ export default function NewAccount({ closeCallback }: { closeCallback: () => voi
             name: description
         }
         addAccount(account)
+        setName("")
+        setDescription("")
+        closeCallback()
     }
 
     return (
